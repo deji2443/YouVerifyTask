d1004d1c1cc78fe3f76d86942ead4726ebc3c9d3
deji2443 <abitech4real@gmail.com>
2026-01-21 10:34:38 +0100

    feat: all login tests passing, fixed reporter config

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'SauceDemo Test Suite',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
-    pageLoadTimeout: 120000,
-    blockHosts: ["*backtrace.io", "*events.backtrace.io"], // Blocks the lagging services
-    retries: {
-      runMode: 2, // Retries the test if the site flakes
-      openMode: 0
-    },
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
+    // This helps resolve pathing issues on MacOS/Linux
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
   },
 });
